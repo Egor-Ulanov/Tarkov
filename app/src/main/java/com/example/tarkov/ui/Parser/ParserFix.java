f153c2c6f613cddaae9766e43bbd820b3a5f4edc
Girich Sergey <girich03@gmail.com>
2023-12-08 18:40:15 +0500

    Реализация сохранения файлов с помощью парсера во внешнем хранилище пользователя

--- app/src/main/java/com/example/tarkov/ui/Parser/ParserFix.java
+++ app/src/main/java/com/example/tarkov/ui/Parser/ParserFix.java
@@ -1,108 +1,65 @@
 package com.example.tarkov.ui.Parser;
 
-
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import android.content.Context;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class ParserFix {
-    private static final String HTML_FILE_PATH = "src/Parser/HotFix/news.html";
-    private static final int UPDATE_INTERVAL_MINUTES = 10;
-    private static final String BASE_URL = "https://www.escapefromtarkov.com/news?lang=ru";
+    private static final String DIRECTORY_NAME = "MyAppDirectoryName";
+    private static final String FILE_NAME = "news.html";
 
-    public static void main(String[] args) {
-        System.out.println("Парсинг новостей EFT...");
-
+    public static List<NewsItem> parseEftNews(Context context) {
         // Загрузка HTML-кода в файл
-        downloadHtmlToFile("https://www.escapefromtarkov.com/news?lang=ru", HTML_FILE_PATH);
+        downloadHtmlToFile(context, "https://www.escapefromtarkov.com/news?lang=ru");
 
         // Парсинг новостей из файла
-        List<NewsItem> newsItems = parseEftNewsFromFile(HTML_FILE_PATH);
+        List<NewsItem> newsItems = parseEftNewsFromFile(context);
 
-        if (newsItems != null && !newsItems.isEmpty()) {
-            System.out.println("Новости EFT успешно спарсены.");
-            // Выводим первые 3 новости
-            for (int i = 0; i < Math.min(3, newsItems.size()); i++) {
-                System.out.println(newsItems.get(i));
-                System.out.println("---------------------");
-            }
-        } else {
-            System.out.println("Новости не были спарсены или произошла ошибка.");
-        }
-
-        // Запускаем таймер для обновления HTML-кода
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                downloadHtmlToFile("https://www.escapefromtarkov.com/news?lang=ru", HTML_FILE_PATH);
-            }
-        }, 0, UPDATE_INTERVAL_MINUTES * 60 * 1000); // Периодически обновляем каждые 10 минут
+        return newsItems;
     }
 
-    public static List<NewsItem> parseEftNews() {
-        try {
-            Document document = Jsoup.connect(BASE_URL)
-                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
-                    .get();
-
-            Elements newsElements = document.select("#news-list .container");
-            List<NewsItem> newsItems = new ArrayList<>();
-
-            int numberOfNews = Math.min(5, newsElements.size());
-            for (int i = 0; i < numberOfNews; i++) {
-                Element newsElement = newsElements.get(i);
-
-                String title = newsElement.select(".headtext a").text();
-                String date = newsElement.select(".headtext span").text();
-                String partialContent = newsElement.select(".description").text();
-                String imageUrl = newsElement.select(".image img").attr("src");
-
-                // Добавляем ссылку на полную новость
-                String fullNewsLink = "https://www.escapefromtarkov.com" + newsElement.select(".headtext a").attr("href");
-
-                NewsItem newsItem = new NewsItem(title, date, partialContent, imageUrl, fullNewsLink);
-                newsItems.add(newsItem);
-            }
-
-            return newsItems;
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.println("Произошла ошибка при парсинге HTML-кода.");
+    private static String getExternalFilePath(Context context) {
+        File directory = new File(context.getExternalFilesDir(null), DIRECTORY_NAME);
+        if (!directory.exists()) {
+            directory.mkdirs(); // Создать директорию, если она не существует
         }
-
-        return null; // Возвращаем null в случае ошибки
+        File file = new File(directory, FILE_NAME);
+        return file.getAbsolutePath();
     }
 
-    // Метод для загрузки HTML-кода в файл
-    public static void downloadHtmlToFile(String url, String filePath) {
+    private static void downloadHtmlToFile(Context context, String url) {
+        String filePath = getExternalFilePath(context);
         try {
             Document document = Jsoup.connect(url)
                     .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
                     .get();
             String htmlContent = document.html();
             File file = new File(filePath);
-            org.apache.commons.io.FileUtils.writeStringToFile(file, htmlContent, "UTF-8");
+
+            FileOutputStream fos = new FileOutputStream(file);
+            OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
+            osw.write(htmlContent);
+            osw.close();
+            fos.close();
         } catch (IOException e) {
             e.printStackTrace();
             System.err.println("Произошла ошибка при обновлении HTML-кода.");
         }
     }
 
-    // Метод для парсинга новостей из HTML-кода
-    public static List<NewsItem> parseEftNewsFromFile(String filePath) {
+    public static List<NewsItem> parseEftNewsFromFile(Context context) {
         List<NewsItem> newsItems = new ArrayList<>();
         try {
-            File input = new File(filePath);
+            File input = new File(getExternalFilePath(context));
             Document document = Jsoup.parse(input, "UTF-8");
 
             Elements newsElements = document.select("#news-list .container");
@@ -131,7 +88,6 @@ public class ParserFix {
         return newsItems;
     }
 
-    // Внутренний класс для представления новости
     public static class NewsItem {
         private String title;
         private String date;
