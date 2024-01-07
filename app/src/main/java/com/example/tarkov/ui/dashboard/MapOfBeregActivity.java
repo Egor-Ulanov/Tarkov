a48d7d42ba8c634c963dacf42cebd5a38fc2543e
Girich Sergey <girich03@gmail.com>
2024-01-07 16:10:23 +0500

    Обновленный метод получения картинок

--- app/src/main/java/com/example/tarkov/ui/dashboard/MapOfBeregActivity.java
+++ app/src/main/java/com/example/tarkov/ui/dashboard/MapOfBeregActivity.java
@@ -1,6 +1,9 @@
 package com.example.tarkov.ui.dashboard;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 import com.example.tarkov.R;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 public class MapOfBeregActivity extends AppCompatActivity {
 
@@ -21,8 +26,12 @@ public class MapOfBeregActivity extends AppCompatActivity {
         imageView = findViewById(R.id.mapImageView);
 
         RadioGroup radioGroup = findViewById(R.id.RadioGroup); // Assuming you have a RadioGroup in your layout
-        imageView.setImage(ImageSource.resource(R.drawable.bereg)); // Set initial image
 
+
+         imageView.setImage(ImageSource.resource(R.drawable.bereg)); // Set initial image
+
+        // Загрузите начальное изображение из Dropbox
+//        loadMapImage("https://www.dropbox.com/scl/fi/nn7f1d4y7wey7jh73ur4a/bereg.png?rlkey=w499vba0n3g7t40m2i10u7al6&dl=1");
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -41,4 +50,28 @@ public class MapOfBeregActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void loadMapImage(String imageUrl) {
+        Picasso.get()
+                .load(imageUrl)
+                .into(new Target() {
+                    @Override
+                    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                        imageView.setImage(ImageSource.bitmap(bitmap));
+                    }
+
+                    @Override
+                    public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+                        // Обработка ошибок загрузки
+                    }
+
+                    @Override
+                    public void onPrepareLoad(Drawable placeHolderDrawable) {
+                        // Подготовка к загрузке
+                    }
+                });
+
+        Log.d("MapOfBeregActivity", "Image loaded: " + imageUrl);
+    }
+
 }
