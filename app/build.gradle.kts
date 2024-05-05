80fdd328e937872ec3ab57cee89eaf6a7e756c86
Girich Sergey <girich03@gmail.com>
2024-05-05 23:54:32 +0500

    Исправление ошибки ограничения памяти, подгрузки всех меток

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,4 +108,7 @@ dependencies {
     // Отправка http Запросов
     implementation ("com.android.volley:volley:1.2.1")
 
+    implementation ("com.github.bumptech.glide:glide:4.14.2")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.14.2")
+
 }
